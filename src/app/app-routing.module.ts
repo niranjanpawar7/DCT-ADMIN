d70badaed7a19559fe3827456f3a0dd4f153a69d
Niranjan Pawar <pkumar@prohaktiv.com>
2019-03-15 13:18:22 +0530

    Added Add venue page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { CreateEventComponent } from '../app/create-event/create-event.component';
 import { ReconcileSpeakerProgramComponent } from '../app/reconcile-speaker-program/reconcile-speaker-program.component'; 
 import { TrainingComponent } from './training/training.component'; 
+import { AddVenueComponent} from './add-venue/add-venue.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/training', pathMatch: 'full' },  
   { path: 'create', component : CreateEventComponent },
   { path: 'reconcile-speaker-program', component : ReconcileSpeakerProgramComponent },
   { path: 'training', component: TrainingComponent},
+  { path : 'add-venue', component: AddVenueComponent},
   { path: '**', component: PageNotFoundComponent },
+  
   
  
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatInputModule } from '@angular/material';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { TrainingComponent, DialogDataExampleDialog } from './training/training.component'; 
 import { ReconcileSpeakerProgramComponent } from './reconcile-speaker-program/reconcile-speaker-program.component';  
+import { AddVenueComponent} from './add-venue/add-venue.component'
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { ReconcileSpeakerProgramComponent } from './reconcile-speaker-program/re
     PageNotFoundComponent, 
     CreateEventComponent, TrainingComponent, 
     DialogDataExampleDialog,  
-    ReconcileSpeakerProgramComponent,  
+    ReconcileSpeakerProgramComponent,
+    AddVenueComponent  
   ],
   imports: [
     BrowserModule,
