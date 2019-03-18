0c48097ab8b88745ad0a40f98cd8e820c64241e3
Akshay katale <akshaykatale99@gmail.com>
2019-03-18 16:51:54 +0530

    final reconcile-speaker-program 6 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,9 @@ import { MatInputModule } from '@angular/material';
 import { CreateEventComponent, createEventPopup } from './create-event/create-event.component';
 import { InitiateTrainingComponent, DialogDateExampleDialog } from './initiate-training/initiate-training.component'; 
 import { TrainingComponent, DialogDataExampleDialog } from './training/training.component'; 
-<<<<<<< HEAD
-import { ReconcileSpeakerProgramComponent, RecSpeakComp, UploadFileModal } from './reconcile-speaker-program/reconcile-speaker-program.component';  
-=======
-import { ReconcileSpeakerProgramComponent } from './reconcile-speaker-program/reconcile-speaker-program.component';  
+import { ReconcileSpeakerProgramComponent, RecSpeakComp, UploadFileModal } from './reconcile-speaker-program/reconcile-speaker-program.component';   
 import { AddVenueComponent} from './add-venue/add-venue.component';
 import { ManageSpkeakerContentComponent, speakerUpload } from './manage-spkeaker-content/manage-spkeaker-content.component'
->>>>>>> 3a181a61b389a9e2b4768d93dc7a8f87ba875f5d
 
 
 @NgModule({
@@ -30,20 +26,15 @@ import { ManageSpkeakerContentComponent, speakerUpload } from './manage-spkeaker
     HeaderComponent,
     FooterComponent,
     PageNotFoundComponent, 
-<<<<<<< HEAD
-    CreateEventComponent, TrainingComponent, InitiateTrainingComponent, 
-    DialogDataExampleDialog,
-    RecSpeakComp,  
-    ReconcileSpeakerProgramComponent, 
-    UploadFileModal 
-=======
-    CreateEventComponent, createEventPopup,
+    CreateEventComponent,
+    RecSpeakComp,   
+    UploadFileModal, 
+    createEventPopup,
     TrainingComponent, InitiateTrainingComponent,DialogDateExampleDialog, 
     DialogDataExampleDialog,  
     ReconcileSpeakerProgramComponent,
     AddVenueComponent,
     ManageSpkeakerContentComponent, speakerUpload 
->>>>>>> 3a181a61b389a9e2b4768d93dc7a8f87ba875f5d
   ],
   imports: [
     BrowserModule,
@@ -58,10 +49,6 @@ import { ManageSpkeakerContentComponent, speakerUpload } from './manage-spkeaker
   ],
   providers: [],
   bootstrap: [AppComponent],
-<<<<<<< HEAD
-  entryComponents: [DialogDataExampleDialog, RecSpeakComp, UploadFileModal]
-=======
-  entryComponents: [DialogDataExampleDialog, createEventPopup, DialogDateExampleDialog, speakerUpload]
->>>>>>> 3a181a61b389a9e2b4768d93dc7a8f87ba875f5d
+  entryComponents: [DialogDataExampleDialog, createEventPopup, DialogDateExampleDialog, speakerUpload, RecSpeakComp, UploadFileModal]
 })
 export class AppModule { }
